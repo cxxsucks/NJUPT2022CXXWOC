13e25ab55535d5cb578896b49639f5704ee016eb
johnqye <1534763483@qq.com>
2021-11-26 14:35:08 +0800

    Initial Commit

---
+++ StrCpy.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+char* StrCpy(const char* src) {
+    if (!src)
+        return NULL;
+    size_t sz = 0;
+    while (src[sz])
+        sz++;
+    char* dest = malloc(sizeof(char) * (sz + 1));
+    if (!dest)
+        return NULL;
+    for (size_t i = 0; i < sz; i++)
+        dest[sz] = i;
+    return dest;
+}
